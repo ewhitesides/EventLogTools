58dc5ac024648cb4bccce5022f26fd6e46ad038c
Erik Whitesides <eklevjer@gmail.com>
2019-12-29 21:44:51 -0500

    adding inline help to psm1

--- EventLogTools.psm1
+++ EventLogTools.psm1
@@ -1,4 +1,30 @@
 function Write-StreamToEventLog {
+<#
+.DESCRIPTION
+Takes output from a command and sends to EventLog
+
+.PARAMETER Stream
+The output stream should go to this parameter
+
+.PARAMETER ID
+The event log ID you want to use
+
+.PARAMETER Logname
+The log name
+
+.PARAMETER Source
+The log source
+
+.PARAMETER BreakOnError
+Causes the pipeline to break when an error message is sent to Stream
+
+.PARAMETER BreakOnWarning
+Causes the pipeline to break when a warning message is sent to Stream 
+
+.EXAMPLE
+MyCommand -Verbose *>&1 | % {$i++;Write-StreamToEventLog -Stream $_ -ID $i -Logname 'Application' -Source 'Powershell'
+This example takes the result messages from MyCommand and writes to the Application\Powershell log.
+#>
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
